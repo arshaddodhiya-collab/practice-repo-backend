d88d840dda8e6d8df5291b51037729b113f9b436
arshaddodhiya-collab <arshad.dodhiya@artemhealthtech.com>
2026-01-29 15:49:55 +0530

    Di example

--- src/main/java/com/test/practice/Car.java
+++ src/main/java/com/test/practice/Car.java
@@ -1,11 +1,13 @@
 package com.test.practice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Car {
-
+    @Autowired
+    Engine engine;
     @GetMapping("/")
     public String home() {
         return "Spring Boot is running ✅";
